1d4308136e88bb02a6c84af3de15c95252e3e4e3
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-11-22 09:31:19 -0600

    modify api for nba; update pngs;

--- src/lib/utils/helperFunctions/nflState.js
+++ src/lib/utils/helperFunctions/nflState.js
@@ -1,15 +1,15 @@
 import { get } from 'svelte/store';
-import {nflState} from '$lib/stores';
+import {nbaState} from '$lib/stores';
 
 export const getNflState = async () => {
-	if(get(nflState).season) {
-		return get(nflState);
+	if(get(nbaState).season) {
+		return get(nbaState);
 	}
-    const res = await fetch(`https://api.sleeper.app/v1/state/nfl`, {compress: true}).catch((err) => { console.error(err); });
+    const res = await fetch(`https://api.sleeper.app/v1/state/nba`, {compress: true}).catch((err) => { console.error(err); });
 	const data = await res.json().catch((err) => { console.error(err); });
 	
 	if (res.ok) {
-		nflState.update(() => data);
+		nbaState.update(() => data);
 		return data;
 	} else {
 		throw new Error(data);
